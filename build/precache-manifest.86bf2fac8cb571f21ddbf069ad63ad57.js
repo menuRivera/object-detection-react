a67fcb7d029221976da4bd9116adf8982e56e7ed
Manuel Rivera <menu.rivera@gmail.com>
2021-11-18 23:07:11 -0600

    modelo agregado crece@cuvalles; build

---
+++ build/precache-manifest.86bf2fac8cb571f21ddbf069ad63ad57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d5a394e8e4e04aedb7a10bf7dc24fa0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddefe0964104917156b6",
+    "url": "/static/css/main.563c1e07.chunk.css"
+  },
+  {
+    "revision": "7aa6d4a1c547be610f6b",
+    "url": "/static/js/2.563f350c.chunk.js"
+  },
+  {
+    "revision": "ddefe0964104917156b6",
+    "url": "/static/js/main.57fc3379.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
