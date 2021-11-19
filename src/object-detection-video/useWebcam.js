760c304e2341fdb6c5987a469f8168a7468b80a0
Manuel Rivera <menu.rivera@gmail.com>
2021-11-19 00:54:46 -0600

    horizontal flip camera

--- src/object-detection-video/useWebcam.js
+++ src/object-detection-video/useWebcam.js
@@ -2,15 +2,16 @@ import { useEffect } from 'react'
 
 const useWebcam = (videoRef, onLoaded) => {
   useEffect(() => {
+    // Checa si hay dispositivos disponibles en el navegador (camaras)
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
       navigator.mediaDevices
-        .getUserMedia({
+        .getUserMedia({// Los solicita
           audio: false,
           video: {
             facingMode: 'environment',
             width: { ideal: 4096 },
             height: { ideal: 2160 },
-          },
+          }, 
         })
         .then((stream) => {
           videoRef.current.srcObject = stream
